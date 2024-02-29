2be017be6a8e51f16f3c72a82bb70204061dea10
Emo0o0 <emil.kolev1102@gmail.com>
2024-02-29 10:49:36 +0200

    restexort/restclient

---
+++ restexport/src/main/java/com/example/sportevents/restexport/SportEventsRestClient.java
@@ -0,0 +1,15 @@
+package com.example.sportevents.restexport;
+
+import com.example.sportevents.api.inputoutput.sportevent.getbyid.GetSportEventByUUIDOutput;
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+import jdk.jfr.ContentType;
+
+@Headers({"Content-Type: application/json"})
+public interface SportEventsRestClient {
+
+    @RequestLine("GET /sportevent/getById/{uuid}")
+    GetSportEventByUUIDOutput getSportEventById(@Param("id") String id);
+
+}
